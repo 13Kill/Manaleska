ef49bc254b2d6733e3432d9fd29ce5a74ce7ebba
Yamashi <yamashi@live.com>
2013-05-09 18:37:49 +0200

    Initial push

---
+++ Src/CardEngine/Logic/Card.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardEngine.Logic
+{
+    class Card
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ Src/CardEngine/Logic/Deck.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardEngine.Logic
+{
+    class Deck
+    {
+        private List<Card> mCards = new List<Card>();
+
+        public Card this[int index]
+        {
+            get
+            {
+                try
+                {
+                    return mCards.ElementAt(index);
+                }
+                catch (System.Exception ex)
+                {
+                	
+                }
+                return null;
+            }
+        }
+    }
+}
