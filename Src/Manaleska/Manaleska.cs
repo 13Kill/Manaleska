9644060c0c641cdb12cbab87d9bea2496f6c8906
Takikun <takikun@gmail.com>
2013-10-08 13:18:04 +0200

    Mouse over menu

--- Src/Manaleska/Manaleska.cs
+++ Src/Manaleska/Manaleska.cs
@@ -1,5 +1,6 @@
 ﻿#region Using Statements
 using System;
+using System.Runtime.InteropServices;
 using System.Collections.Generic;
 using System.Net.Mime;
 using Microsoft.Xna.Framework;
@@ -17,10 +18,21 @@ namespace Manaleska
     /// </summary>
     public class Manaleska : Game
     {
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern uint MessageBox(IntPtr hWnd, String text, String caption, uint type); // Pour pouvoir utiliser les MessageBox
+
         GraphicsDeviceManager graphics;
         SpriteBatch _spriteBatch;
-        Texture2D button;
-        Vector2 buttonPosition = new Vector2 (20, 40);
+        Texture2D btnNewGame, btnOptions, btnExit;
+        Vector2 btnNewGamePosition = new Vector2((GraphicsDeviceManager.DefaultBackBufferWidth/2)-75, 70); // tout ça pour centrer le bouton, bidouille impacto
+        Vector2 btnOptionsPosition = new Vector2((GraphicsDeviceManager.DefaultBackBufferWidth / 2) - 75, 130);
+        Vector2 btnExitPosition = new Vector2((GraphicsDeviceManager.DefaultBackBufferWidth / 2) - 75, 190);
+        Rectangle btnNewGameArea = new Rectangle((GraphicsDeviceManager.DefaultBackBufferWidth / 2) - 75, 70, 150, 50); // La surface où se situe le bouton "Nouvelle Partie"
+        Rectangle btnOptionsArea = new Rectangle((GraphicsDeviceManager.DefaultBackBufferWidth / 2) - 75, 130, 150, 50);
+        Rectangle btnExitArea = new Rectangle((GraphicsDeviceManager.DefaultBackBufferWidth / 2) - 75, 190, 150, 50);
+        bool overNewGame = false;
+        bool overOptions = false;
+        bool overExit = false;
 
 
         public Manaleska()
@@ -58,7 +70,9 @@ namespace Manaleska
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-            this.button = Content.Load<Texture2D>("button");
+            this.btnNewGame = Content.Load<Texture2D>("newGame");
+            this.btnOptions = Content.Load<Texture2D>("options");
+            this.btnExit = Content.Load<Texture2D>("exit");
             // TODO: use this.Content to load your game content here
         }
 
@@ -79,6 +93,13 @@ namespace Manaleska
         protected override void Update(GameTime gameTime)
         {
             MouseState mouse = Mouse.GetState();
+            Point mousePosition = new Point(mouse.X, mouse.Y);
+            if (btnNewGameArea.Contains(mousePosition)) overNewGame = true;
+                else overNewGame = false;
+            if (btnOptionsArea.Contains(mousePosition)) overOptions = true;
+                else overOptions = false;
+            if (btnExitArea.Contains(mousePosition)) overExit = true;
+                else overExit = false;
    
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
@@ -98,7 +119,16 @@ namespace Manaleska
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             _spriteBatch.Begin();
-            _spriteBatch.Draw(this.button, this.buttonPosition, Color.White);
+
+            if (!overNewGame) _spriteBatch.Draw(this.btnNewGame, this.btnNewGamePosition, Color.White);
+                else _spriteBatch.Draw(Content.Load<Texture2D>("overNewGame"), this.btnNewGamePosition, Color.White);
+
+            if(!overOptions) _spriteBatch.Draw(this.btnOptions, this.btnOptionsPosition, Color.White);
+                else _spriteBatch.Draw(Content.Load<Texture2D>("overOptions"), this.btnOptionsPosition, Color.White);  
+                   
+            if (!overExit)_spriteBatch.Draw(this.btnExit, this.btnExitPosition, Color.White);
+                else _spriteBatch.Draw(Content.Load<Texture2D>("overExit"), this.btnExitPosition, Color.White);   
+
             _spriteBatch.End();
 
             base.Draw(gameTime);
