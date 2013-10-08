5da28ea98ef9a504509bed29cf6ee231baeff683
Takikun <takikun@gmail.com>
2013-10-08 03:09:56 +0200

    Fix

--- Src/Manaleska/Manaleska.cs
+++ Src/Manaleska/Manaleska.cs
@@ -28,7 +28,7 @@ namespace Manaleska
         }
 
         /// <summary>
-        ///     Allows the game to perform any initialization it needs to before starting to run.
+        ///     Allows the game to perform any initialization it needs to before starting to run..
         ///     This is where it can query for any required services and load any non-graphic
         ///     related content.  Calling base.Initialize will enumerate through any components
         ///     and initialize them as well.
